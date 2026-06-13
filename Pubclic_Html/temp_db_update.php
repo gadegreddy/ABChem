1b84f0be3ffe8878790eed2e986f896730d395da
Kishore Gade <54764669+gadegreddy@users.noreply.github.com>
2026-06-13 16:47:47 +0530

    Delete Pubclic_Html/temp_db_update.php

--- Pubclic_Html/temp_db_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../private/functions.php';
-try {
-    $db = Database::getInstance();
-    $db->query("UPDATE compounds SET applications = NULL");
-    echo "Success! The applications column has been cleared.";
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
